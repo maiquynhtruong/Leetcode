eac48ed3d4bbbb4809d4bc2a123b981fc46d0a72
Mai Truong <maitruong2801@gmail.com>
2016-12-22 14:05:56 -0800

    Create palindrome-number.java

---
+++ palindrome-number.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x != 0 && x % 10 == 0)) return false;
+        int n = x, len = 1, p = 1;
+        
+        while (n > 9) {
+            p *= 10; len++; n /= 10;   
+        }
+        // p = p / 10;
+        for (int i = 0; i < len / 2; i++) {
+            long last = x % 10;
+            long first = x / p;
+            if (last != first) {
+                return false;
+            }
+            x =  x % p;
+            x =  x / 10;
+            p /= 100;
+        }
+        return true;
+    }
+}
