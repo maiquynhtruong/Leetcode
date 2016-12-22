717c39f4c7b298392f44bc1c70318adfbe5c4592
Mai Truong <maitruong2801@gmail.com>
2016-12-22 14:11:23 -0800

    Update palindrome-number.java

--- palindrome-number.java
+++ palindrome-number.java
@@ -20,3 +20,18 @@ public class Solution {
         return true;
     }
 }
+
+/**
+Best solution
+public class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x != 0 && x % 10 == 0)) return false;
+        int rev = 0;
+        while (x > rev) {
+            rev = rev * 10 + x % 10;
+            x = x / 10;
+        }
+        return ((x == rev) || (x == rev / 10));
+    }
+}
+**/
