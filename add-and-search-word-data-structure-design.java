04a7e16f1b070acfd0a2abd036c205f0f0181ea2
Mai Truong <maitruong2801@gmail.com>
2016-12-23 23:05:44 -0800

    Update add-and-search-word-data-structure-design.java
    Accepted

--- add-and-search-word-data-structure-design.java
+++ add-and-search-word-data-structure-design.java
@@ -1,12 +1,11 @@
 public class WordDictionary {
 
-    public TrieNode 
-    {
+    public class TrieNode {
         TrieNode links[];
-        isWord = false;
+        boolean isWord = false;
         
         public TrieNode() {
-            links =  new links[26];
+            links =  new TrieNode[26];
             for (int i = 0; i < 26; i++) links[i] = null;
         }
     }
@@ -14,43 +13,47 @@ public class WordDictionary {
     // Adds a word into the data structure.
     public void addWord(String word) {
         TrieNode cur = head;
-        for (int i = 0; i < word.length(); i++)
-        {
+        for (int i = 0; i < word.length(); i++) {
             int index = word.charAt(i) - 'a';
-            if (cur.links[index] == null) 
-            {
-                cur = new TrieNode();
+            if (cur.links[index] == null) {
+                cur.links[index] = new TrieNode();
             }
             cur = cur.links[index];
         }
+        cur.isWord = true;
     }
+    
 
     // Returns if the word is in the data structure. A word could
     // contain the dot character '.' to represent any one letter.
     public boolean search(String word) {
-        TrieNode cur = head;
-        for (int i = 0; i < word.length(); i++) 
-        {
-            if (word.charAt(i) == '.')
-            {
-                for (int j = 0; j < 26; j++) 
-                {
-                    if (cur.links[j] != null)
-                    {
-                        cur = cur.links[j];
-                    }
-                }
-                
-            } 
-            else 
-            {
-                int index = word.charAt(i) - 'a';
-                if (cur.links[index] != null) 
-                {
-                    cur = cur.links[index];
-                }
-            }
+        return searchFrom(word, head);
+    }
+        
+    public boolean searchFrom(String word, TrieNode root) {
+        TrieNode cur = root;
+        for (int i = 0; i < word.length(); i++) {
+        	// System.out.println(word.charAt(i));
+        	if (cur != null) {
+        		
+	            if (word.charAt(i) == '.') {
+	                for (int j = 0; j < 26; j++) {
+	                	// System.out.println(j + " " + cur.links[j]);
+                        if (searchFrom(word.substring(i+1), cur.links[j]))
+                            return true;
+	                }
+                cur = null;
+	            } else {
+	                int index = word.charAt(i) - 'a';
+	               // if (cur.links[index] != null) {
+	                    cur = cur.links[index];
+	               // }
+	            }
+        	} else {
+        		break;
+        	}
         }
+        return cur != null && cur.isWord;		
     }
 }
 
