1e5c475d9318632d5ea21baf184d7f7bc9a3e563
Mai Truong <maitruong2801@gmail.com>
2016-12-25 20:58:59 -0800

    Update remove-duplicates-from-sorted-array.java

--- remove-duplicates-from-sorted-array.java
+++ remove-duplicates-from-sorted-array.java
@@ -12,3 +12,19 @@ public class Solution {
         return curPos;
     }
 }
+
+/** 
+Better solution
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length < 2) return nums.length;
+        int curPos = 1;
+        for (int i = 1; i < nums.length; i++) {
+        	if (nums[i] != nums[i-1]) {
+        		nums[curPos++] = nums[i];
+        	} 
+        }
+        return curPos;
+    }
+}
+**/
