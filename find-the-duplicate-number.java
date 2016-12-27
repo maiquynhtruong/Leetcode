ce5c889a30abb4a095c79a013d02cc71c2009b2f
Mai Truong <maitruong2801@gmail.com>
2016-12-26 20:29:31 -0800

    Create find-the-duplicate-number.java

---
+++ find-the-duplicate-number.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int findDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i-1]) {
+                return nums[i];
+            }
+        }
+        return nums[0];
+    }
+}
