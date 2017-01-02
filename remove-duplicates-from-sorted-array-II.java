75068fad5e95008fb19cde1eab12f030a4740b7b
Mai Truong <maitruong2801@gmail.com>
2017-01-01 19:25:02 -0800

    Create remove-duplicates-from-sorted-array-II.java

---
+++ remove-duplicates-from-sorted-array-II.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Ideone m = new Ideone();
+		int[] nums = new int[]{1,1,1,1,1};
+		System.out.println(m.removeDuplicates(nums));
+		for (int i = 0; i < nums.length; i++) 
+			System.out.print(nums[i] + " " );
+	}
+ 
+	public int removeDuplicates(int[] nums) {
+		if (nums.length < 3) return nums.length;
+        int i1 = 0, i2 = 1, cur = 2;
+        while(cur < nums.length) {
+        	if (nums[i1] == nums[i2]) {
+        		while (cur < nums.length && nums[cur] == nums[i2]) cur++;
+        		System.out.println("cur= " + cur);
+        		if (cur < nums.length) {nums[i2+1] = nums[cur++]; i1++;i2++;} 
+        	} else {
+        		nums[++i2] = nums[cur++];
+        		i1++; 
+        	}
+        }
+        return i2+1;
+	}
+}
