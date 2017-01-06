d8e2760864b00d89c999613503c9402a209ac09a
Mai Truong <maitruong2801@gmail.com>
2017-01-05 19:20:36 -0800

    Update encode-string-with-shortest-length.java
    This works

--- encode-string-with-shortest-length.java
+++ encode-string-with-shortest-length.java
@@ -9,32 +9,31 @@ class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		String s = "aabcaabcd";
+		String s = "abcdeabcde";
 		System.out.println(encode(s));
 	}
 	
-	public static String encode (String s) {
+	public static String encode(String s) {
 		int n = s.length();
-		String dp[][] = new String[n][n]; // substring from i to j in encoded form
-		for (int l = 0; l < n; l++)
+		String dp[][] = new String[n][n];
+		for (int l = 0; l < n; l++) {
 			for (int i = 0; i < n-l; i++) {
-				int j = i+l;
+				int j = i + l;
 				String sub = s.substring(i, j+1);
 				if (sub.length() < 5) {
 					dp[i][j] = sub;
 				} else {
 					dp[i][j] = sub;
 					for (int k = i; k < j; k++) {
-						if (dp[i][j].length() < dp[i][k].length() + dp[k+1][j].length()) {
+						if (dp[i][j].length() > dp[i][k].length() + dp[k+1][j].length()) {
 							dp[i][j] = dp[i][k] + dp[k+1][j];
 						}
 					}
-					
 					for (int k = 0; k < sub.length(); k++) {
 						String repeat = sub.substring(0,k+1);
 						if (repeat != null && sub.length()%repeat.length() == 0
 						&& sub.replaceAll(repeat, "").length() == 0) {
-							String shorter = sub.length()/repeat.length() + "[" + 
+							String shorter = sub.length()/repeat.length() + "[" +
 							dp[i][i+k] + "]";
 							if (shorter.length() < dp[i][j].length()) {
 								dp[i][j] = shorter;
@@ -43,6 +42,7 @@ class Ideone
 					}
 				}
 			}
+		}
 		return dp[0][n-1];
 	}
 }
