9b8e8848b62b69208103636bea2c5c3c343e1465
Mai Truong <maitruong2801@gmail.com>
2017-01-09 10:42:22 -0800

    Update find-peak-element.java

--- find-peak-element.java
+++ find-peak-element.java
@@ -8,3 +8,30 @@ public class Solution {
         return nums.length-1;
     }
 }
+
+/** O(logN) **/
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int[] nums = new int[] {1,2,3,4,5,6,7,3,1};
+		System.out.println(findPeakElement(nums));
+	}
+	public static int findPeakElement(int[] nums) {
+		int low = 0, high = nums.length-1;
+		while (low < high) {
+			int mid = (low + high) / 2;
+			System.out.println("low= " + low + ", high= " + high + ", mid= " + mid);
+			if (nums[mid] < nums[mid+1]) {
+				low = mid+1;
+			} else {
+				high = mid;
+			}
+		}
+		return low;
+	}
+}
+
