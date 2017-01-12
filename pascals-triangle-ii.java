2372861826d377f9a31068adcaaafc3c63541a82
Mai Truong <maitruong2801@gmail.com>
2017-01-12 11:28:56 -0800

    Create pascals-triangle-ii.java

---
+++ pascals-triangle-ii.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        /**
+         * [1]
+         * [1,1]
+         * [1,2,1]
+         * [1,3,3,1]
+         **/
+        List<Integer> res = new ArrayList<Integer>();
+        List<Integer> helper = new ArrayList<Integer>();
+        helper.add(1);
+        for (int i = 1; i <= rowIndex; i++) {
+            res.add(1);
+            for (int j = 1; j < helper.size(); j++) {
+                res.add(helper.get(j) + helper.get(j-1));
+            }
+            res.add(1);
+            helper = res;
+            res = new ArrayList<Integer>();
+        }
+        return helper;
+    }
+}
