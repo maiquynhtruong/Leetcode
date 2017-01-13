751c9b4d0d62f57d1a5531780128bdebf98c8c85
Mai Truong <maitruong2801@gmail.com>
2017-01-12 16:15:58 -0800

    Update contains-duplicate-ii.java

--- contains-duplicate-ii.java
+++ contains-duplicate-ii.java
@@ -13,4 +13,14 @@ public class Solution {
         }
         return false;
     }
+    
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashSet<Integer> hs = new HashSet<Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (i > k) hs.remove(nums[i-k-1]);
+            if (!hs.add(nums[i])) return true;
+        }
+        
+        return false;
+    }
 }
