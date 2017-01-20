d175cd3391a8a8154afc4745e3b627671df95c05
Mai Truong <maitruong2801@gmail.com>
2017-01-20 11:48:19 -0500

    Update range-sum-query-immutable.java

--- range-sum-query-immutable.java
+++ range-sum-query-immutable.java
@@ -1,3 +1,20 @@
+// Top solution using partial sums
+public class NumArray {
+    int[] nums;
+    public NumArray(int[] nums) {
+       for (int i = 1; i < nums.length; i++) {
+           nums[i] = nums[i-1] + nums[i];
+       }
+       this.nums = nums;
+    }
+    
+    public int sumRange(int i, int j) {
+        if (i == 0) return nums[j];
+        return nums[j] - nums[i-1];
+    }
+}
+
+// Tried implementing Segmented Tree but failed horribly
 public class NumArray {
 
     int[] tree;
