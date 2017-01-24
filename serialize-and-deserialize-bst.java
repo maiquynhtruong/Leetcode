8537336bdb96cdf3b7d6d771f80e9e76d0b62c07
Mai Truong <maitruong2801@gmail.com>
2017-01-23 20:08:58 -0500

    Update serialize-and-deserialize-bst.java

--- serialize-and-deserialize-bst.java
+++ serialize-and-deserialize-bst.java
@@ -1,4 +1,4 @@
-/* package whatever; // don't place package name! */
+/** This works but got TLE**/
 
 import java.util.*;
 import java.lang.*;
@@ -83,3 +83,57 @@ class Ideone
     	return root;	
     }
 }
+
+/** This is the best solution. Very elegant too**/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Codec {
+    public static String splitter = ",";
+	public static String nl = "X";
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+		StringBuilder sb = new StringBuilder();
+		buildString(sb, root);
+		return sb.toString();
+	}
+	public void buildString(StringBuilder sb, TreeNode root) {
+		if (root == null) {
+			sb.append(nl).append(splitter);
+		} else {
+			sb.append(root.val).append(splitter);
+			buildString(sb, root.left);	
+			buildString(sb, root.right);
+		}
+	}
+	
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        List<String> l = Arrays.asList(data.split(splitter));
+		Deque<String> d = new LinkedList(l);
+		return buildTree(d);
+	}
+	public TreeNode buildTree(Deque<String> d) {
+		String val = d.remove();
+		if (val.equals(nl)) {
+			return null;
+		} else {
+			TreeNode root = new TreeNode(Integer.parseInt(val));
+			root.left = buildTree(d);
+			root.right = buildTree(d);
+			return root;
+		}
+	}
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.deserialize(codec.serialize(root));
+
