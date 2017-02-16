dd5d6158ab2d7cc47564fb7970362c95826238e4
Mai Truong <maitruong2801@gmail.com>
2017-02-15 20:30:47 -0500

    Update evaluate-division.java

--- evaluate-division.java
+++ evaluate-division.java
@@ -18,26 +18,33 @@ class Ideone
 		double[] results = o.calcEquation(equations, values, queries);
 		System.out.println(Arrays.toString(results));
 	}
+	
+	
 	public double[] calcEquation(String[][] equations, double[] values, String[][] queries) {
-    	HashMap<String, HashMap<String, Double>> hm = new HashMap<String, HashMap<String, Double>>();
-    	for (int i = 0; i < equations.length; i++) {
-    		String[] strs = equations[i];
-    		if (!hm.containsKey(strs[0])) {
-    			HashMap<String, Double> sub = new HashMap<String, Double>();
-    			hm.put(strs[0], sub);
-    		}
-			hm.get(strs[0]).put(strs[1], values[i]);
-			if (!hm.containsKey(strs[1])) {
+		HashMap<String, HashMap<String, Double>> hm = new HashMap<String, HashMap<String, Double>>();
+		for (int i = 0; i < equations.length; i++) {
+			String[] strs = equations[i];
+			if (!hm.containsKey(strs[0])) {
 				HashMap<String, Double> sub = new HashMap<String, Double>();
-				hm.put(strs[1], sub);
+				hm.put(strs[0], sub);
 			}
-			hm.get(strs[1]).put(strs[1], 1/values[i]);
-    	}
-    	double[] results = new double[queries.length];
-    	for (int i = 0; i < queries.length; i++) {
-    		HashMap<String, Double> sub = hm.get(queries[i][0]);
-    		results[i] = sub.get(queries[i][1]);
-    	}
-    	return results;
-    }
+				hm.get(strs[0]).put(strs[1], values[i]);
+				if (!hm.containsKey(strs[1])) {
+					HashMap<String, Double> sub = new HashMap<String, Double>();
+					hm.put(strs[1], sub);
+				}
+				hm.get(strs[1]).put(strs[1], 1/values[i]);
+		}
+		double[] results = new double[queries.length];
+		for (int i = 0; i < queries.length; i++) {
+		    if (hm.containsKey(queries[i][0])) {
+			    HashMap<String, Double> sub = hm.get(queries[i][0]);
+			    if (sub.containsKey(queries[i][1])) results[i] = sub.get(queries[i][1]);
+			    else results[i] = -1.0;
+		    } else {
+			    results[i] = -1.0;
+		    }
+		}
+		return results;   
+	    }
 }
