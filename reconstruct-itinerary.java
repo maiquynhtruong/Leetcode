308ee852c6eed0547e5969226f733f26aaa82e0e
Mai Truong <maitruong2801@gmail.com>
2017-02-17 11:12:51 -0500

    Update reconstruct-itinerary.java

--- reconstruct-itinerary.java
+++ reconstruct-itinerary.java
@@ -7,15 +7,22 @@ public class Solution {
             }
             flights.get(tickets[i][0]).add(tickets[i][1]);
         }
-        for (int i = 0; i < tickets.length; i++) {
-            if (flights.get()
-        }
+        ArrayList<String> flightOrder = new ArrayList<String>();
+        System.out.println();
+        dfs("JFK", flightOrder, flights, tickets.length+1);
+        flightOrder.add(0,"JFK");
+        return flightOrder;
     }
-    public void dfs(String flight, ArrayList<String> flightOrder, HashMap<String, PriorityQueue<String>> flights) {
+    public void dfs(String flight, ArrayList<String> flightOrder, HashMap<String, PriorityQueue<String>> flights, int len) {
+        if (flightOrder.size() == len) return;
+        if (!flights.containsKey(flight) || flights.isEmpty()) return;
         PriorityQueue<String> connecting = flights.get(flight);
-        flightOrder.add(connecting.pop());
-        for (String next : connecting) {
-            
+        System.out.println(Arrays.toString(connecting.toArray()));
+        while (!connecting.isEmpty()) {
+            String nextFlight = connecting.poll();
+            dfs(nextFlight, flightOrder, flights, len);
+            flightOrder.add(0, nextFlight);
         }
+        
     }
 }
