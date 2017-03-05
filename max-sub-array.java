f0bfca6ed82e8320486483b6e9b13f9993ece2a7
Mai Truong <maitruong2801@gmail.com>
2017-03-05 07:20:52 -0800

    Create max-sub-array.java

---
+++ max-sub-array.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int maxSubArray(int[] nums) {
+        int dp[] = new int[nums.length];
+        dp[0] = nums[0];
+        int max = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (dp[i-1] > 0) dp[i] = dp[i-1] + nums[i];
+            else dp[i] = nums[i];
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+}
