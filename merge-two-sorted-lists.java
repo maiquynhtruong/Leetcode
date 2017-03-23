27615f7b3839898d44e02095f484833fea89702a
Mai Truong <maitruong2801@gmail.com>
2017-03-22 23:35:26 -0700

    Add recursive method

--- merge-two-sorted-lists.java
+++ merge-two-sorted-lists.java
@@ -34,4 +34,17 @@ public class Solution {
         }
         return head.next;
     }
+    
+    // Recursively
+    public ListNode mergeTwoListsRecursively(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoListsRecursively(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoListsRecursively(l1, l2.next);
+            return l2;
+        }
+    }
 }
