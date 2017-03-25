8589f3ea1ebde23f79d381e407874a17a62b6969
Mai Truong <maitruong2801@gmail.com>
2017-03-25 13:08:22 -0700

    Update increasing-subsequences.java

--- increasing-subsequences.java
+++ increasing-subsequences.java
@@ -1,22 +1,19 @@
 public class Solution {
     public List<List<Integer>> findSubsequences(int[] nums) {
         int n = nums.length;
-        List<List<Integer>> ans = new ArrayList<List<Integer>>();
-        for (int i = 0; i < n; i++) {
-            for (int len = 2; len <= n - i; len++) {
-                int pivot = 1, int index = i;
-                List<Integer> temp = new ArrayList<Integer>();
-                temp.add(nums[index++]);
-                while (index < n && pivot < len) {
-                    if (nums[index] > nums[index-1]) {
-                        temp.add(nums[index]);
-                        pivot++;
-                    }
-                    index++;
-                }
-                ans.add(temp);
+        Set<List<Integer>> ans = new HashSet<List<Integer>>();
+        List<Integer> temp = new ArrayList<Integer>();
+        findSubsequencesHelper(nums, ans, temp, 0);
+        return new ArrayList(ans);
+    }
+    public void findSubsequencesHelper(int[] nums, Set<List<Integer>> ans, List<Integer> temp, int index) {
+        if (temp.size() >= 2) ans.add(new ArrayList(temp));
+        for (int i = index; i < nums.length; i++) {
+            if (temp.size() == 0 || temp.get(temp.size()-1) <= nums[i]) {
+                 temp.add(nums[i]);
+                 findSubsequencesHelper(nums, ans, temp, index+1);
+                 temp.remove(temp.size()-1);
             }
         }
-        return ans;
     }
 }
