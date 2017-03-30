6b7cf6116101ce8440745b10ac24c485cf0d7cca
Mai Truong <maitruong2801@gmail.com>
2017-03-29 22:50:57 -0700

    Create jump_game.java

---
+++ jump_game.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public boolean canJump(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int ind = i; ind < i + nums[i]; ind++) {
+                if (i + nums[i]
+            }
+        }
+    }
+}
