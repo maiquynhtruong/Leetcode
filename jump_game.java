6b8a54199439cc1e1a7582d726022524fcf651d2
Mai Truong <maitruong2801@gmail.com>
2017-03-31 09:36:59 -0700

    Update jump_game.java

--- jump_game.java
+++ jump_game.java
@@ -1,7 +1,10 @@
 public class Solution {
     public boolean canJump(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            reach = (i + nums[i]);
+        int reach = 0;
+        int n = nums.length;
+        for (int i = 0; i < n && i <= reach; i++) {
+            reach = Math.max(i + nums[i], reach);
         }
+        return reach >= nums[n-1]; // or i == n;
     }
 }
