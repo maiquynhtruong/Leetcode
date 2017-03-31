38700e689de025bd4721b0a68ccc123c80315101
Mai Truong <maitruong2801@gmail.com>
2017-03-30 22:50:44 -0700

    Update jump_game.java

--- jump_game.java
+++ jump_game.java
@@ -1,9 +1,7 @@
 public class Solution {
     public boolean canJump(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
-            for (int ind = i; ind < i + nums[i]; ind++) {
-                if (i + nums[i]
-            }
+            reach = (i + nums[i]);
         }
     }
 }
