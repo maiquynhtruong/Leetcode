a27e4945657b1ee1e1d551364a61bf9f55ccb026
Mai Truong <maitruong2801@gmail.com>
2017-04-06 21:44:46 -0700

    Update regular-expression-matching.java

--- regular-expression-matching.java
+++ regular-expression-matching.java
@@ -1,16 +1,18 @@
 public class Solution {
     public boolean isMatch(String s, String p) {
         if (s == null || p == null) return false;
+        boolean dp[][] = new boolean[s.size()+1][p.size()+1];
         for (int i = 0; i < s.size(); i++) 
             for (int j = 0; j < p.size(); j++) {
-                if (p.charAt(j) == s.charAt(i)) dp[i][j] = dp[i-1][j-1];
-                if (p.charAt(i) == '.') dp[i][j] = dp[i-1][j-1]; // '.' can replace any character
+                if (p.charAt(j) == s.charAt(i)) dp[i+1][j+1] = dp[i][j];
+                if (p.charAt(i) == '.') dp[i+1][j+1] = dp[i][j]; // '.' can replace any character
                 if (p.charAt(i) == '*') {
-                    if (p.charAt(j-1) != s.charAt(i)) dp[i][j] = dp[i][j-2];
-                    else if (p.charAt(j-1) != s.charAt(i) || p.charAt(j-1) == '.') {
-                        
+                    if (p.charAt(j-1) != s.charAt(i) && p.charAt(j-1) == '.') dp[i+1][j+1] = dp[i+1][j-1];
+                    else {
+                        dp[i+1][j+1] = dp[i+1][j-1] || dp[i][j+1] || dp[i+1][j];
                     }
                 }
             }
+        return dp[s.size()][p.size()];
     }
 }
