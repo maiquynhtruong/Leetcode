f6aa67072292edbbbd43f71360ca0e1147b58dbd
Mai Truong <maitruong2801@gmail.com>
2017-04-19 15:01:35 -0400

    My understand version, number theory did not work

--- student-attendance-record-ii.java
+++ student-attendance-record-ii.java
@@ -1,10 +1,15 @@
 public class Solution {
     public int checkRecord(int n) {
-        int unrewardable = 0;
-        for (int cnt = 2; cnt <= n; cnt++)
-            unrewardable += (n-cnt+1) * ((int) Math.pow(3, n-cnt));
-        for (int cnt = 3; cnt <= n; cnt++) 
-            unrewardable += (n-cnt+1) * ((int) Math.pow(3, n-cnt));
-        return ((int) Math.pow(3, n)) - unrewardable;
+        // dp[i][a][l] = number of valid sequence with length i, at most a absences and at most l lates
+        int[][][] dp = new int[n+1][2][3];
+        dp[0] = {{1,1,1}, {1,1,1}}; // 0 length, everything is 1
+        for (int i = 1; i <= n; i++) 
+            for (int j = 0; j < 2; j++)
+                for (int k = 0; k < 3; k++) {
+                    int value = dp[i-1][j][k]; // P for present
+                    if (j > 0) value = (value + dp[i-1][j-1][2]) % Integer.MAX_VAL;
+                    if (k > 0) value = (value + dp[i-1][1][k-1]) % Integer.MAX_VAL;
+                }
+        return dp[n][1][2];
     }
 }
