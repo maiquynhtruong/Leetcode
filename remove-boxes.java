76aab73365c3492d9fa39fa6db9149af928e3f8d
Mai Truong <maitruong2801@gmail.com>
2017-04-30 13:30:01 -0700

    Update remove-boxes.java

--- remove-boxes.java
+++ remove-boxes.java
@@ -8,9 +8,10 @@ public class Solution {
         if (l > r) return 0;
         if (dp[l][r][k] != 0) return dp[l][r][k];
         while (r > 1 && boxes[r] == boxes[r-1]) {r--; k++;}
-        dp[l][r][k] = dfs(l, r-1, 0) + (k+1)*(k+1);
+        dp[l][r][k] = dfs(boxes, dp, l, r-1, 0) + (k+1)*(k+1);
         for (int i = l; i < r; i++)
-            if (boxes[i] == boxes[r]) dp[l][r][k] = Math.max(dp[l][r][k], dp[l][i][k+1] + dp[i+1][r][0]);
+            if (boxes[i] == boxes[r]) 
+                dp[l][r][k] = Math.max(dp[l][r][k], dfs(boxes, dp, l, i, k+1) + dfs(i+1, r, 0));
         return dp[l][r][k];
     }
     
