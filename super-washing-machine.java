24fb5aba42a294e2884b8c2669863395d34d4c05
Mai Truong <maitruong2801@gmail.com>
2017-05-03 13:44:09 -0400

    Update super-washing-machine.java

--- super-washing-machine.java
+++ super-washing-machine.java
@@ -3,17 +3,15 @@ public class Solution {
         int n = machines.length;
         int[] sum = new int[n+1];
         for (int i = 1; i <= n; i++) sum[i] = sum[i-1] + machines[i-1];
-        int average = 0;
+        int average = 0, res = 0;
         if (sum[n] % n != 0) return -1;
         else average = sum[n] / n;
-        int[] dp = new int[n+1];
         for (int i = 0; i < n; i++) {
             int left = i * average - sum[i];
-            int right = (n-i+1) * average - (sum[n] - sum[i]);
-            if (left > 0 && right > 0) dp[i+1] = dp[i] + Math.abs(left) + Math.abs(right);
-            else if (left < 0 && right < 0) dp[i+1] = dp[i] + Math.max(Math.abs(left), Math.abs(right));
-            else [i+1] = dp[i] + Math.max(Math.abs(left), Math.abs(right));
+            int right = (n-i-1) * average - (sum[n] - sum[i] - machines[i]);
+            if (left > 0 && right > 0) res = Math.max(res, Math.abs(left) + Math.abs(right));
+            else res = Math.max(res, Math.max(Math.abs(left), Math.abs(right)));
         }
-        return dp[n];
+        return res;
     }
 }
