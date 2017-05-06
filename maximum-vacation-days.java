9f34d7d2e83173f03dc34464380e33428d7ecf9a
Mai Truong <maitruong2801@gmail.com>
2017-05-05 22:30:29 -0700

    Update maximum-vacation-days.java

--- maximum-vacation-days.java
+++ maximum-vacation-days.java
@@ -2,15 +2,25 @@ public class Solution {
     public int maxVacationDays(int[][] flights, int[][] days) {
         int n = flights.length;
         int k = days[0].length;
-        int[][] dp = new int[n][k];
-        for (int src = 0; src < n; src++) {
+        // int[][] dp = new int[n][k];
+        // Since the values of week i only depend on the the values of week i-1, we can compress two-dimensional dp array into one dimensional
+        int[] dp = new int[n];
+        Arrays.fill(dp, Integer.MIN_VALUE);
+        for (int week = 0; week < k; week++) {
+            int[] temp = new int[n];
+            Arrays.fill(temp, Integer.MIN_VALUE);
             for (int des = 0; des < n; des++) {
-                for (int week = 0; week < k; week++) {
-                    if (flight[src][des]) dp[des][week] = Math.max(dp[des][week], dp[src][week-1] + days[des][week]);
+                for (int src = 0; src < n; src++) {
+                    if (flight[src][des] || src == des) temp[des] = Math.max(temp[des], dp[src] + days[des][week]);
                 }
             }
+            dp = temp;
         }
-        
+        int max = 0;
+        for (int i : dp) {
+            max - Math.max(max, i);
+        }
+        return max;
     }
 }
     
