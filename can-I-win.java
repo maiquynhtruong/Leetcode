8b855ab9f52ea195cc6ff1d605cbac73d95b8404
Mai Truong <maitruong2801@gmail.com>
2017-05-13 20:59:35 -0700

    Update can-I-win.java

--- can-I-win.java
+++ can-I-win.java
@@ -1,22 +1,28 @@
 import java.util.*;
 public class Solution {
+    HashMap<Integer, Boolean> hm;
+    boolean[] used;
     public boolean canIWin(int maxChoosableInteger, int desiredTotal) {
         int sum = (maxChoosableInteger+1) * maxChoosableInteger / 2;
         if (sum < desiredTotal) return false;
         if (desiredTotal <= 0) return true;
-        HashMap<Integer, Boolean> hm = new HashMap<Integer, Boolean>();
-        return canFirstWin(desiredTotal, 0, hm);
+        hm = new HashMap<Integer, Boolean>();
+        used = new boolean[maxChoosableInteger+1];
+        return canFirstWin(desiredTotal);
     }
     
-    public boolean canWin(int total, boolean[] used, HashMap<Integer, Boolean> hm) {
-        if (total < 0) return false;
-        for (int i = 1; i <= MaxChoosableInteger; i++) 
-            if (!used[i]) {
-                used[i] = true;
-                if (canWin(total - i, ))
-                    hm.put(, true);
-                used[i] = false;
-            }
+    public boolean canWin(int total) {
+        if (total <= 0) return false;
+        int usedArray = format(used);
+        if (!hm.containsKey(usedArray)) {
+            for (int i = 1; i <= used.length; i++) 
+                if (!used[i]) {
+                    used[i] = true;
+                    if (canWin(total - i, ))
+                        hm.put(, true);
+                    used[i] = false;
+                }
+        }
     }
     
     public int binaryToInteger(boolean[] used) {
