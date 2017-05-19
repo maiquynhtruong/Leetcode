04b77cbe66143f3e3ade1df03927bbe1a22b69ca
Mai Truong <maitruong2801@gmail.com>
2017-05-19 13:29:37 -0400

    Update out_of_boundary_paths.java

--- out_of_boundary_paths.java
+++ out_of_boundary_paths.java
@@ -1,5 +1,21 @@
 public class Solution {
     public int findPaths(int m, int n, int N, int i, int j) {
+        int numPath = 0;
+    }
+    public void countWays(int m, int n, int N, int times, int i, int j) {
+        if (times > N) return 0;
+        if (!okToGo(m, n, i, j)) {
+            numPath++;
+            return;
+        }
+        countWays(m, n, N, times+1, i-1, j);
+        countWays(m, n, N, times+1, i+1, j);
+        countWays(m, n, N, times+1, i, j-1);
+        countWays(m, n, N, times+1, i, j+1);
         
+    }
+    public boolean okToGo(int m, int n, int i, int j) {
+        if (i > -1 && i < m && j > -1 && j < n) return true;
+        else return false;
     }
 }
