1449cf37acf747d07cf33a7ccd88948c0647c407
Mai Truong <maitruong2801@gmail.com>
2017-05-19 14:00:28 -0400

    Update out_of_boundary_paths.java

--- out_of_boundary_paths.java
+++ out_of_boundary_paths.java
@@ -1,21 +1,12 @@
 public class Solution {
     public int findPaths(int m, int n, int N, int i, int j) {
-        int numPath = 0;
-    }
-    public void countWays(int m, int n, int N, int times, int i, int j) {
-        if (times > N) return 0;
-        if (!okToGo(m, n, i, j)) {
-            numPath++;
-            return;
-        }
-        countWays(m, n, N, times+1, i-1, j);
-        countWays(m, n, N, times+1, i+1, j);
-        countWays(m, n, N, times+1, i, j-1);
-        countWays(m, n, N, times+1, i, j+1);
-        
-    }
-    public boolean okToGo(int m, int n, int i, int j) {
-        if (i > -1 && i < m && j > -1 && j < n) return true;
-        else return false;
+        int dp[][][] = new int[51][50][50];
+        for (int ni = 0; ni <= N; i++) 
+            for (int ii = 0; ii < m; ni++)
+                 for (int ji = 0; ji < n; ji++) {
+                    dp[ni][ii][ji] = ((ii == 0 ? 1 : dp[ni-1][ii-1][ji]) + (ii == m ? 1 : dp[ni-1][ii+1][ji]) + 
+                                      (ji == 0 ? 1 : dp[ni-1][ii][ji-1] + (ji == n ? 1 : dp[ni-1][ii][ji+1]));
+                 }
+        return dp[N][m-1][n-1];
     }
 }
