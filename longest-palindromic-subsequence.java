167c09c0369316a691c44491929ff0d482fd036e
Mai Truong <maitruong2801@gmail.com>
2017-05-19 12:12:44 -0400

    the finish version

--- longest-palindromic-subsequence.java
+++ longest-palindromic-subsequence.java
@@ -2,13 +2,15 @@ public class Solution {
     public int longestPalindromeSubseq(String s) {
         int len = s.size();
         int dp[][] = new int[len][len];
-        for (int i = 0; i < len; i++)
-            for (int j = 0; j < i; j++) {
+        for (int i = len-1; i >= 0; i--) {
+            dp[i][i] = 1;
+            for (int j = i+1; j < len; j++) {
                 if (s.charAt(i) == s.charAt(j)) dp[i][j] = dp[i+1][j-1] + 2;
                 else {
                     dp[i][j] = Math.max(dp[i][j-1], dp[i+1][j]);
                 }
             }
-        return dp[0][len];
+        }
+        return dp[0][len-1];
     }
 }
