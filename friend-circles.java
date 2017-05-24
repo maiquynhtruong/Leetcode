640f9d9ae9b3e51dfe83a92332c7d9047a614b6f
Mai Truong <maitruong2801@gmail.com>
2017-05-24 17:18:20 -0400

    Update friend-circles.java

--- friend-circles.java
+++ friend-circles.java
@@ -1,23 +1,30 @@
 public class Solution {
+    int[] parent = new int[];
+    int count;
     public int findCircleNum(int[][] M) {
-        HashSet<Integer> friendGraph = new HashSet<Integer>();
+        for (int i = 0; i < parent.length; i++) {
+            parent[i] = i;
+        }
+        count = M.length;
         for (int i = 0; i < M.length; i++)
-            for (int j = 0; j < M[0].length; j++)
-                if (M[i][j] == 1 && i != j) {
-                    friendGraph.add(i);
-                    friendGraph.add(j);
-                }
-        return friendGraph.size();
+            for (int j = 0; j < M[0].length; j++) {
+                if (M[i][j] == 1) union(i, j);
+            }
+        return count;
     }
     
     public void union(int i, int j) {
         int root_i = find(i);
         int root_j= find(j;
+        if (root_i == root_j) return;
+        parent[root_i] = parent[root_j];
+        count--;
     }
     public int find(int i) {
         while (i != parents[i]) {
-            parent[p = parent[parent[i]];
-            p = parent[i];
+            parent[i = parent[parent[i]];
+            i = parent[i];
         }
+        return i;
     }
 }
