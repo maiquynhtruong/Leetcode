81d595ebd64945ad70d7aea3ddde92110603c1c2
Mai Truong <maitruong2801@gmail.com>
2017-06-02 11:47:42 -0400

    some examples

--- reverse-pairs.java
+++ reverse-pairs.java
@@ -1,5 +1,12 @@
 public class Solution {
     public int reversePairs(int[] nums) {
+        int dp[] = new int[nums.length];
         
     }
 }
+
+/*
+2, 1, 4, 3, 20, 31, 27, 23, 13, 12, 15, 4, 5, 6, 7, 11
+1  0  0  0  5   8   7   5   3   2   4   0  0  0  0  0
+0  1  0  0  0   0   0   0   2   2   1   7  7  6  5  2
+*/
