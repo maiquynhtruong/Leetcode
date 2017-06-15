2840318e0e1da170cb91e4121f2d2eaa6dc3bee0
Mai Truong <maitruong2801@gmail.com>
2017-06-15 15:58:10 -0400

    Create lowest-common-ancestor.java

---
+++ lowest-common-ancestor.java
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // while (can still find both nodes in the subtree of the current node) {
+        // try the left
+        // try the right
+        
+    }
+}
