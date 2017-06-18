fdc4c51bc899dbc92aada1ac6e5e118fda0c4e8b
Mai Truong <maitruong2801@gmail.com>
2017-06-17 20:24:36 -0700

    Update remove-k-digits.java

--- remove-k-digits.java
+++ remove-k-digits.java
@@ -1,25 +1,25 @@
 public class Solution {
     public String removeKdigits(String num, int k) {
-        "1432219", k = 3 -> "1219"
-        Stack<Integer> s = new Stack<Integer>();
-        int i = 0;
-        while (i < n && s.size() < n-k ) {
+        int digits = num.length() - k;
+        int[] s = new int[digits];
+        int top = 0;
+        for (int i = 0; i < num.length(); i++)
             int cur = Character.getNumericValue(num.charAt(i));
-            if (s.peek() > cur) {
-                s.pop(); s.push(cur);
+            while (top > 0 && s[top-1] > c && k > 0) {
+                top--;
+                k--;
             }
-            
+            s[top++] = cur;
         }
-
+        i = 0;
+        while (i < digits && s[i] == '0') i++;
+        return i == digits ? "0" : new String(s, i, digits - i);
+        "143", k = 3 -> "0"
+        "1432", k = 3 -> "1"
+        "14323", k = 3 -> "12"
+        "143236", k = 3 -> "123"
+        "1432365", k = 3 -> "1235"
+        "14323659", k = 3 -> "12359"
+        "14323659"
     }
 }
-
-/**
-        "143", k = 3 -> "0"
-        "132", k = 3 -> "1"
-        "123", k = 3 -> "12"
-        "123", k = 3 -> "123"
-        "1235", k = 3 -> "1235"
-        "12359", k = 3 -> "12359"
-        
-*/
