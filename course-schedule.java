dd7495a0703a7cdfa14690bef0549a96c7e1fb94
Mai Truong <maitruong2801@gmail.com>
2017-06-24 13:28:55 -0700

    Update course-schedule.java

--- course-schedule.java
+++ course-schedule.java
@@ -1,5 +1,13 @@
 public class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        Set<Integer> set = new HashSet<Integer>();
+        while (!set.empty()) {
+            int v = set.pop();
+            for (int i : getVertices(v)) {
+                if (neighbors[i].contains(v))set.push(i);   
+            }
+            list.add(v);
+        }
     }
 }
