7e74b9fc4ac128202a7470aba0b1958e4ba5f540
Mai Truong <maitruong2801@gmail.com>
2017-06-26 15:44:03 -0400

    Create rotate-list.java

---
+++ rotate-list.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        ListNode p1 = head, p2 = head;
+        while (k--) {
+            p1 = p1.next;
+        }
+        while (p1.next != null) {
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        p1.next = head;
+        return p2;
+    }
+}
