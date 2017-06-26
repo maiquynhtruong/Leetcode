4347ccf6629956354be66594b7fe01010353842a
Mai Truong <maitruong2801@gmail.com>
2017-06-26 15:20:35 -0400

    Update merge-k-sorted-lists.java

--- merge-k-sorted-lists.java
+++ merge-k-sorted-lists.java
@@ -15,6 +15,24 @@ import java.io.*;
 /* Name of the class has to be "Main" only if the class is public. */
 class Ideone
 {
+	public ListNode mergeKLists(ListNode[] lists) {
+		PriorityQueue<ListNode> pq = new PriorityQueue<lists.size(), new Comparator<ListNode>() {
+		    public int compare(ListNode node1, ListNode node2) {
+			return node1.val - node2.val;
+		    }  
+		}
+		for (ListNode list : lists) {
+		    if (list != null) pq.add(list);
+		}
+		ListNode head = new ListNode(0);
+		ListNode pointer = head.next;
+		while (!pq.isEmpty()) {
+		    tail.next = pq.poll();
+		    tail = tail.next;
+
+		    if (tail.next != null) pq.add(tail.next);
+		}
+	}
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		return mergeLists(lists, 0, lists.length-1);
