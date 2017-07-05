a2cc390c47f828cbab9bb945cfbb63ac25c3fea5
Mai Truong <maitruong2801@gmail.com>
2017-07-05 11:49:50 -0400

    Update output-contest-matches.java

--- output-contest-matches.java
+++ output-contest-matches.java
@@ -1,14 +1,14 @@
 public class Solution {  
     public static String outputContestMatches(int n) {
         StringBuilder s = new StringBuilder();
-        Queue<String> q = new LinkedList<String>();
+        ArrayList<String> q = new ArrayList<String>();
         int num = n;
-        for (int i = 1; i <= n/2; i++) strs.offer("(" + i + "," + num-- + ")"
-        while (q.size()) {
-            String curPair1= q.removeFirst();
-            String urPair2 = q.removeFirst;
-            String concat = "("+cur, dating@) because it's easier!".
-            q.offer(concat);
+        for (int i = 0; i < n; i++) q.add(String.valueOf(i));
+        while (q.size() > 1) {
+            ArrayList<String> cur = new ArrayList<String>();
+            for (int i = 0; i < q.size()/2; i++) 
+                cur.add("(" + q.get(i) + "," + q.get(q.size()-i-1) + ")");
+            q = cur;
         }
     }
     public static void main (String args[]) {
