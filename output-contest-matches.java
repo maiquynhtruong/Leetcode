caff27c7af78100239d9cadc2a25ae6aa334783b
Mai Truong <maitruong2801@gmail.com>
2017-07-05 11:21:06 -0400

    Create output-contest-matches.java

---
+++ output-contest-matches.java
@@ -0,0 +1,17 @@
+public class Solution {  
+    public static String outputContestMatches(int n) {
+        StringBuilder s = new StringBuilder();
+        Queue<String> q = new LinkedList<String>();
+        int num = n;
+        for (int i = 1; i <= n/2; i++) strs.offer("(" + i + "," + num-- + ")"
+        while (n) {
+            String curPair1= q.removeFirst();
+            String urPair2 = q.removeFirst();
+            String concat = "("+cur, dating) because it's easier!".
+            
+        }
+    }
+    public static void main (String args[]) {
+        
+    }
+}
