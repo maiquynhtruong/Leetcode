5dc60499c605a78e2936124fc86567563a00a4c5
Mai Truong <maitruong2801@gmail.com>
2018-02-05 00:09:47 -0500

    Update shortest-completing-word.cpp

--- shortest-completing-word.cpp
+++ shortest-completing-word.cpp
@@ -1,29 +1,40 @@
 class Solution {
 public:
-    string shortestCompletingWord(string licensePlate, vector<string>& words) {
-        int charSize = 'z' + 1;
-        int freq[charSize], licenseFreq[charSize];
-        set<char> plateChars;
-        for (int i = 0; i < licensePlate.size(); i++) {
-            if (isalpha(licensePlate[i])) licenseFreq[tolower(licensePlate[i])]++;
-            plateChars.insert(tolower(licensePlate[i]));
+    struct compare {
+        bool operator()(const std::string& first, const std::string& second) {
+            return first.size() < second.size();
         }
-        for (int i = 0; i < words.size(); i++) {
-            if (compareStrings(freq, words[i])) return words[i];
-        }
-        return "";
-    }
-    
-    
-    
-    bool compareStrings(int freq[], int licenseFreq[], string word, set<char> &plateChars;) {
-        for (int i = 0; i < word.size(); i++) {
-            freq[word[i]]++;
-        }
+    };
+    bool compareStrings(int freq[], int licenseFreq[], string word, set<char> &plateChars) {
+        for (int i = 'a'; i <= 'z'; i++) freq[i] = 0;
+        for (int i = 0; i < word.size(); i++) freq[word[i]]++;
+        
         for (set<char>::iterator itr = plateChars.begin(); itr != plateChars.end(); ++itr)
         {
+            //cout << *itr << " " << freq[*itr] << " " << licenseFreq[*itr] << endl;
             if (freq[*itr] < licenseFreq[*itr]) return false;
         }
         return true;
     }
+    string shortestCompletingWord(string licensePlate, vector<string>& words) {
+        int charSize = 'z' + 1;
+        int freq[charSize], licenseFreq[charSize];
+        set<char> plateChars;
+        for (int i = 'a'; i <= 'z'; i++) licenseFreq[i] = 0;
+        for (int i = 0; i < licensePlate.size(); i++) {
+            if (isalpha(licensePlate[i])) {
+                licenseFreq[tolower(licensePlate[i])]++;
+                plateChars.insert(tolower(licensePlate[i]));
+            }
+        }
+        compare c;
+        sort(words.begin(), words.end(), c);
+        for (int i = 0; i < words.size(); i++) {
+            cout << words[i] << "\n";
+            if (compareStrings(freq, licenseFreq, words[i], plateChars)) return words[i];
+            //cout << "\n";
+        }
+        return "";
+    }
+
 };
