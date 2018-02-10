bb5002c73814df76a9d727990b3f02c1d4b6403f
Mai Truong <maitruong2801@gmail.com>
2018-02-10 12:36:27 -0500

    Update house-robber.java

--- house-robber.java
+++ house-robber.java
@@ -4,8 +4,8 @@ class Solution {
         if (n == 0) return 0;
         if (n == 1) return nums[0];
         int[] robbed = new int[n];
-        for (int i = 0; i < n; i++) robbed[i] = nums[i];
-        for (int i = 0; i < n; i++) {
+        robbed[0] = nums[0]; robbed[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < n; i++) {
             robbed[i] = Math.max(nums[i] + robbed[i-2], robbed[i-1]);
             //robbed[i] = Math.max(robbed[i], robbed[i-1] + robbed[i+1]);    
         }
