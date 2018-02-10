4987261748264558687501795e0fef3c6a56a448
Mai Truong <maitruong2801@gmail.com>
2018-02-10 13:11:35 -0500

    Update delete-and-earn.java

--- delete-and-earn.java
+++ delete-and-earn.java
@@ -1,16 +1,19 @@
 class Solution {
     public int deleteAndEarn(int[] nums) {
-        HashMap<Integer, Integer> numCnt = new HashMap<Integer, Integer>();
-        int maxSum = 0, sum = 0;
+        if (nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+        if (nums.length == 2) return Math.max(nums[0], nums[1]);
+        int maxNum = 0;
+        for (int i = 0; i < nums.length; i++) maxNum = Math.max(maxNum, nums[i]);
+        int[] points = new int[maxNum+1];
+        int[] earn = new int[nums.length];
         for (int i = 0; i < nums.length; i++) {
-            numCnt.putIfAbsent(nums[i], numCnt.getOrDefault(nums[i], 0) + 1);
-            sum += nums[i];
+            points[nums[i]] += nums[i];
         }
-        for (int i = 0; i < nums.length; i++) {
-            int newSum = sum - nums[i];
-            newSum = newSum - numCnt.getOrDefault(nums[i]-1, 0) * (nums[i]-1) - numCnt.getOrDefault(nums[i]+1, 0) * (nums[i]+1);
-            maxSum = Math.max(newSum, maxSum);
+        earn[0] = points[0]; earn[1] = Math.max(points[0], points[1]);
+        for (int i = 1; i < points.length; i++) {
+            earn[i] = Math.max(nums[i] + earn[i-2], earn[i-1]);
         }
-        return maxSum;
+        return Math.max(earn[points.length-1], earn[points.length-2]);
     }
 }
