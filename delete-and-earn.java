f2a6be8f8b64f33ddebeb0be1beb133b939a2305
Mai Truong <maitruong2801@gmail.com>
2018-02-10 13:20:32 -0500

    Update delete-and-earn.java

--- delete-and-earn.java
+++ delete-and-earn.java
@@ -1,19 +1,14 @@
 class Solution {
     public int deleteAndEarn(int[] nums) {
-        if (nums.length == 0) return 0;
-        if (nums.length == 1) return nums[0];
-        if (nums.length == 2) return Math.max(nums[0], nums[1]);
-        int maxNum = 0;
-        for (int i = 0; i < nums.length; i++) maxNum = Math.max(maxNum, nums[i]);
-        int[] points = new int[maxNum+1];
-        int[] earn = new int[nums.length];
+        if (nums.length == 0) return 0;        
+        int[] points = new int[10005], earn = new int[nums.length];
         for (int i = 0; i < nums.length; i++) {
             points[nums[i]] += nums[i];
         }
         earn[0] = points[0]; earn[1] = Math.max(points[0], points[1]);
-        for (int i = 1; i < points.length; i++) {
-            earn[i] = Math.max(nums[i] + earn[i-2], earn[i-1]);
+        for (int i = 2; i < 10001; i++) {
+            earn[i] = Math.max(earn[i] + earn[i-2], earn[i-1]);
         }
-        return Math.max(earn[points.length-1], earn[points.length-2]);
+        return earn[10000];
     }
 }
