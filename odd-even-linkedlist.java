efd9cd986bdef50108afae705af6ecbb7da75e09
Mai Truong <maitruong2801@gmail.com>
2018-06-03 21:42:05 -0400

    add another way

--- odd-even-linkedlist.java
+++ odd-even-linkedlist.java
@@ -18,3 +18,26 @@ public class Solution {
         return head;
     }
 }
+
+/** Another way **/
+
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode dummy = new ListNode(-1), tummy = new ListNode(-1);
+        dummy.next = head; tummy.next = head.next;
+        ListNode odd = dummy, even = tummy, cur = head;
+        while (cur != null) {
+            
+            odd.next = cur;
+            odd = odd.next;
+            cur = cur.next;
+            even.next = cur;
+            if (cur == null) break;
+            even = even.next;
+            cur = cur.next;
+        }
+        odd.next = tummy.next;
+        return dummy.next;
+    }
+}
