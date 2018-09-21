26fc14cfe20ffe477bd5b222658b2d046c9138e8
Mai Truong <maitruong2801@gmail.com>
2018-09-21 00:22:06 -0400

    Update lowest-common-ancestor-of-a-binary-tree.java

--- lowest-common-ancestor-of-a-binary-tree.java
+++ lowest-common-ancestor-of-a-binary-tree.java
@@ -17,3 +17,35 @@ class Solution {
     }
     
 }
+
+/*** brute force way, but still pass **/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while (true) {
+            if (checkNode(root.left, p) && checkNode(root.left, q)) {
+                root = root.left;
+            } else if (checkNode(root.right, p) && checkNode(root.right, q)) {
+                root = root.right;
+            } else {
+                break;
+            }
+        }
+        return root;
+    }
+    
+    public boolean checkNode(TreeNode root, TreeNode p) {
+        if (root == null) return false;
+        if (root == p) return true;
+        return checkNode(root.left, p) || checkNode(root.right, p);
+    }
+}
